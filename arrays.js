5e9ba827316220aa03a3388e2fa4f08d7eb29b00
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-16 11:12:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,43 +4,48 @@ var chocolateBars = [
   "kitkat",
   "skittles"
   ]
-  
-function addElementToBeginningOfArray(array, item){
-  return [item, ...array]
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, item){
-  array.unshift(item);
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
   return array
 }
 
-function addElementToEndOfArray(array, item){
-  return [...array, item]
+function addElementToEndOfArray(array, element){
+  return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray(array, item){
-  array.push(item);
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
   return array
 }
 
 function accessElementInArray(array, index){
-   return array[index]
- }
-
-function destructivelyRemoveElementFromBeginningOfArray(array){
-   array.shift();
-   return array
- }
-
-function removeElementFromBeginningOfArray(array){
-   return array.slice(1)
+  return array[index]
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
   return array
 }
 
-function removeElementFromEndOfArray(array) {
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function destructivelyRemoveELementFromEndOfArray(array){
+  return array.slice(-1)
+}
+
+function removeElementFromEndOfArray(array){
   return array.slice(0, array.length - 1)
 }
